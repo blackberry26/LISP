f39a7b1887d4c1c06efdb002263644e76ab9b87f
blackberry26 <43415122+blackberry26@users.noreply.github.com>
2019-03-14 09:39:59 +0200

    Create 10.lisp

---
+++ 10.lisp
@@ -0,0 +1,20 @@
+;10.Определите функцию, осуществляющую удаление указанного количества по-
+следних элементов исходного списка.
+
+(defun dell(arr n)
+	(defun leave (arr n)
+		(if (= n 0)
+			'()
+			(cons (car arr) (leave (cdr arr) (- n 1)))
+		)
+	)
+	
+	(leave arr (- (length arr) n))
+)
+
+test case:
+in:(print(dell '(1 2 3 4 5) 3))
+out:(1 2)
+
+in:(print(dell '(1 2 3 4 5) 5))
+out:NIL
