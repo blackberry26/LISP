76b4461bda96187ed01b6351957e5443d0d0f37f
blackberry26 <43415122+blackberry26@users.noreply.github.com>
2019-03-20 17:39:50 +0300

    Update 7.lisp

--- 7.lisp
+++ 7.lisp
@@ -1,16 +1,18 @@
 ;7.Определите функцию, удаляющую из исходного списка элементы с четными номерами.
 
-(defun del(lst)
-	(cond
-		((null (cddr lst)) (cons (car lst) (cddr lst)))
-		(t (cons (car lst) (del (cddr lst) )))
-	)
+(defun divide (lst)
+   ((lambda (head tail)
+         (if head
+                (cons head (divide (cdr tail)))
+         )  
+   ) 
+    (car lst)
+    (cdr lst)
+  )
 )
 
-test case:
 
-in:(print(del '(1 2 3 4 )))
-out:(1 3)
-
-in:(print(del '(1 5 67 3 8 )))
-out:(1 67 8)
+int:(print (divide '(1 2 3 4)))
+out: (1 3)
+int:(print (divide '(1 2 3 4 1 2 2 4))) 
+out: (1 3 1 2)
