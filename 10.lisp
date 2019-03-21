e6758f5f0359a691a9ac44141d508c9c463a14fe
blackberry26 <43415122+blackberry26@users.noreply.github.com>
2019-03-21 09:28:42 +0200

    Update 10.lisp

--- 10.lisp
+++ 10.lisp
@@ -1,9 +1,19 @@
 ;10.Определите функцию, осуществляющую удаление указанного количества последних элементов исходного списка.
 
     (defun delend(list)                      
-    (cond                                          
-    ((null (cdr list)) nil)                        
-    (t (cons (car list) (delend (cdr list))))))
+      (cond      
+          
+        ((null (cdr list)) nil)        
+          
+          (t (cons (car list) (delend (cdr list))
+                   
+             )
+             
+          )
+          
+       )
+        
+    )
 
 test case:
 in:(print(delend '(9 6 5)))
