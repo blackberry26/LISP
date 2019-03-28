df46f272c829072cdda7851af3ba192a968c88be
blackberry26 <43415122+blackberry26@users.noreply.github.com>
2019-03-28 09:32:31 +0200

    Update 22.lisp

--- 22.lisp
+++ 22.lisp
@@ -1,13 +1,14 @@
 ;24.Определите функцию, которая обращает список (а b с) и разбивает его на уровни (((с) b) а).
 
-(defun reverse-abc (x)
-	(list(list (cddr x) (cadr x)) (car x))
-)
+(defun rev (lst)
+  (if (null (cdr lst))
+      lst
+      (list (rev (cdr lst)) (car lst))))
 
 test case:
 
-in:(print(reverse-abc '(1 2 3 4 5)))
+in:(print(rev '(1 2 3 4 5)))
 out:(((((5) 4) 3) 2) 1) 
 
-in:(print(reverse-abc '(1 2 5)))
+in:(print(rev '(1 2 5)))
 out:(((5) 2) 1) 
