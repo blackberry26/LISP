64037c9dccf7c1f13d79798961e33c2e15650e60
blackberry26 <43415122+blackberry26@users.noreply.github.com>
2019-03-28 08:48:43 +0200

    Update 11.lisp

--- 11.lisp
+++ 11.lisp
@@ -1,30 +1,17 @@
 ;11.Определите функцию, осуществляющую разделение исходного списка на два подсписка. В первый из них должно попасть указанное
 ;количество элементов с начала списка, во второй — оставшиеся элементы.
 
-(defun F (lst N)
-  (if lst
-    (if (zerop N)
-      (cons nil (cons lst nil)     
-     )
-        
-      ((lambda (elem result)     
-         (cons
-           (cons elem (car result)      
-           )
-             (cdr result)
-         )
-      )
-       
-            (car lst)    
-            (F (cdr lst) (- N 1))     
-       )       
-     )    
-   )  
- )
+(defun divide (list length)
+    (if list
+        (if (zerop length) (cons nil (cons list nil))
+            ((lambda (elem result)
+                     (cons (cons elem (car result))
+                           (cdr result)))
+             (car list)
+(divide (cdr list) (- length 1))))))
 
-test case:
 
-in:(print(F '(1 2 3 4 5)2))
+in:(print(divide '(1 2 3 4 5)2))
 out:((1 2)(3 4 5))
-in:(print(F '(1 2 3 4 5)1))
+in:(print(divide '(1 2 3 4 5)1))
 out:((1)(2 3 4 5))
