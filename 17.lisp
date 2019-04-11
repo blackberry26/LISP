08ae696ca0f8b7e764637b5b312cf3e41a7e3968
blackberry26 <43415122+blackberry26@users.noreply.github.com>
2019-04-11 09:58:35 +0300

    Update 17.lisp

--- 17.lisp
+++ 17.lisp
@@ -3,9 +3,9 @@
 (defun permutation (lst)
     
     (cond
-        ( (null lst) nil)
+        ( (null lst) nil) 
         ( (null (cdr lst) ) (list lst))
-        ( t (add-elem-to-each-permutation (car lst) (permutation (cdr lst) ) ) )
+        ( t (add-elem-to-each-permutation (car lst) (permutation (cdr lst) ) ) ) ;  вставляем 1ый элемент в каждую перест-ку хвостовой части
     )
 )
 
@@ -14,8 +14,8 @@
     (cond
         ( (null perm-lst) nil)
         (t (append
-                (insert-elem-to-each-pos elem (car perm-lst))
-                (add-elem-to-each-permutation elem (cdr perm-lst) )  
+                (insert-elem-to-each-pos elem (car perm-lst)) ; вставляем эл-т в каждую перест-ку для головы
+                (add-elem-to-each-permutation elem (cdr perm-lst) ) ; соединяем с хвостовым списком 
 ) 
         )
      )
@@ -25,9 +25,9 @@
     
     (cond
         ( (null lst) (list elem) )
-        ( (atom lst) (insert-elem-to-each-pos elem (list lst) ) )
+        ( (atom lst) (insert-elem-to-each-pos elem (list lst) ) );если lst  атомарный, то оборачиваем
         ( t (cons 
-             (cons elem lst) (insert-elem-to-each-pos-aux elem nil lst) ) ) 
+             (cons elem lst) (insert-elem-to-each-pos-aux elem nil lst) ) ) ; если нет, то соединяем вставленный эл-т
     )
 )
 
@@ -36,7 +36,7 @@
     (cond
         ( ( null lst2) nil)
         ( t ( (lambda (a)
-                (cons (append (car a) (list elem) (cadr a))
+                (cons (append (car a) (list elem) (cadr a)) ; вставляем "а" между 2мя списками
                     
                       ( (lambda (x)
                         (insert-elem-to-each-pos-aux elem (car x) (cadr x)))
@@ -52,5 +52,5 @@
 )
 
 
-(print (permutation '(1 2 3)))
-;>((1 2 3) (1 3 2) (2 1 3) (2 3 1) (3 1 2) (3 2 1))
+;(print (permutation '(1 2 3)))
+;((1 2 3) (1 3 2) (2 1 3) (2 3 1) (3 1 2) (3 2 1))
