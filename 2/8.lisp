9804362558a241535590f967a095873776a6a91c
blackberry26 <43415122+blackberry26@users.noreply.github.com>
2019-04-18 09:57:58 +0200

    Create 8.lisp

---
+++ 2/8.lisp
@@ -0,0 +1,17 @@
+;8. Написать генератор натуральных чисел
+
+(defun nature-generator()
+	(let ((num 0))
+		(lambda () (setq num (+ num 1)))
+	)
+)
+
+(defun main ()
+	(setq gen1 (nature-generator))
+	(setq gen2 (nature-generator))
+  (list (funcall gen2) (funcall gen2) (funcall gen1) (funcall gen2) (funcall gen1) (funcall gen2)(funcall gen2)
+                                                                                   (funcall gen2)(funcall ge1))
+)
+
+;(print(main))
+;(1 2 1 3 2 4 5 6 3) 
