aa8bc3cf2e0c0fcbfb62a3a11e6ac3770024c12e
blackberry26 <43415122+blackberry26@users.noreply.github.com>
2019-05-14 18:50:55 +0300

    Update and rename 1.lisp to 1.hs

--- haskell/1.hs
+++ haskell/1.hs
@@ -1,7 +1,7 @@
 ;1.Реализовать на языке Haskell функцию вычисления суммы элементов списка
 
 sum1 [] = 0
-sum1 (a:aTail) = a + sum1 aTail
+sum1 (h:t) = h + sum1 t
 
 main = print $ sum1 [1, 2, 3]
 ;6
