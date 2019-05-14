fbd6f017a8edf0c8271bcfafa622c2ed76cb63cc
blackberry26 <43415122+blackberry26@users.noreply.github.com>
2019-05-14 18:14:31 +0300

    Update 12.lisp

--- 2/12.lisp
+++ 2/12.lisp
@@ -1,8 +1,8 @@
 ;12.Определите функцию, которая возвращает в качестве значения свой вызов.
 
 
-(defun return_self (&rest rest)
-	(append '(return_self) rest)
+(defun return_self (&rest arg)
+	(append '(return_self) arg)
 )
 
 (print (return_self 1 2)) ;(RETURN_SELF 1 2) 
