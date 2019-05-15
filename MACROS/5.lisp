916e54cabe16190acdc5b34ee35d86d0b2de210c
blackberry26 <43415122+blackberry26@users.noreply.github.com>
2019-05-15 19:19:18 +0300

    Create 5.lisp

---
+++ MACROS/5.lisp
@@ -0,0 +1,21 @@
+; 5. Определите в виде макроса форму (REPEAT e UNTIL p) паскалевского типа. 
+(defmacro repeat (e until p)
+	`(if ,p NIL
+		(progn ,e (repeat ,e until ,p))
+	)
+)
+
+(setq i 0)
+(repeat
+	(progn
+		(print i)
+		(setq i (+ i 1))
+	)
+	until (= i 5)
+)
+
+;0 
+;1 
+;2 
+;3 
+;4 
